7a4d3b1a4f510a0a5c1fb3a54bb78d116b2426e0
clojure ninja <clojureninja@gmail.com>
2010-07-14 21:54:48 -0400

    adding latest of bonus files

--- clj-from-java/src/main/clojure/capclug/demo/cljfromjava/eleven.clj
+++ clj-from-java/src/main/clojure/capclug/demo/cljfromjava/eleven.clj
@@ -1,1 +1,7 @@
-(ns capclug.demo.cljfromjava.eleven)
+(ns capclug.demo.cljfromjava.eleven
+  (:gen-class
+    :methods [[foo [String "[Ljava.lang.String;"] String]]))
+
+; compiles to String[] b/c we explicitly said to do so above
+(defn -foo [str & args]
+  "confused?")
--- clj-from-java/src/main/clojure/capclug/demo/cljfromjava/twelve.clj
+++ clj-from-java/src/main/clojure/capclug/demo/cljfromjava/twelve.clj
@@ -1,1 +1,6 @@
-(ns capclug.demo.cljfromjava.twelve)
+(ns capclug.demo.cljfromjava.twelve
+  (:gen-class))
+
+; not smart enought to compile & args to an array... it is just java.lang.Object
+(defn -foo [str & args]
+  "had enough?")
